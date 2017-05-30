d70a6ac8d29b96073dc01c74f15dd11622e72452
Ben Foxall <benfoxall@gmail.com>
2017-05-31 00:49:02 +0100

    WORDS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {ExpressPeerServer} = require('peer')
 app.use(express.static('static'))
 app.use(express.static('node_modules/milligram/dist'))
 
-const server = app.listen(9000 || process.env.port)
+const server = app.listen(9000 || process.env.PORT)
 
 const options = {
     debug: true
