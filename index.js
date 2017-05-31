f8ad2f461bd0d42a31c3b6e64a49d72bddb90bf2
Ben Foxall <benfoxall@gmail.com>
2017-05-31 06:37:38 +0100

    initial connection

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const {ExpressPeerServer} = require('peer')
 
 app.use(express.static('static'))
 app.use(express.static('node_modules/milligram/dist'))
+app.use(express.static('node_modules/peerjs/dist'))
+
+app.get('/broadcast', (req, res) => res.sendFile(__dirname + '/static/broadcast.html'))
+app.get('/connect', (req, res) => res.sendFile(__dirname + '/static/connect.html'))
 
 const server = app.listen(process.env.PORT || 9000)
 
@@ -11,4 +15,4 @@ const options = {
     debug: true
 }
 
-app.use('/api', ExpressPeerServer(server, options))
+app.use('/peer', ExpressPeerServer(server, options))
